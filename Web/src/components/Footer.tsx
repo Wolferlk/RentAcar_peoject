bb5630645eec261b3d92f1c9374bc68a657585f0
Kanchana Koralage <itsmekanchanakoralage@gmail.com>
2025-07-23 15:20:30 +0530

    Add button to footer for login as an admin

--- Web/src/components/Footer.tsx
+++ Web/src/components/Footer.tsx
@@ -69,6 +69,7 @@ const Footer: React.FC = () => {
                 <MapPin className="w-4 h-4 text-blue-400" />
                 <span className="text-gray-400">123 Main St, Downtown</span>
               </div>
+              <Link to="" className="flex items-center space-x-3">Login as an Admin</Link>
             </div>
           </div>
 
