2fcbce7109d3d075467dbc874a1e01d287297ea2
Rishini Nawodi <123630889+rishininawodi@users.noreply.github.com>
2025-08-06 22:32:58 +0530

    Adding Routers to GetApprovedowners

--- backend/Routers/Admin/admin-ownerRouter.js
+++ backend/Routers/Admin/admin-ownerRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getPendingOwners, approveOwner, rejectOwner } = require('../../controllers/Admin/admin-ownerController');
+const { getPendingOwners, approveOwner, rejectOwner,getApprovedOwners} = require('../../controllers/Admin/admin-ownerController');
 const { verifySuperAdminToken } = require('../../middleware/Auth/verifyToken');
 const { isSuperAdmin } = require('../../middleware/Auth/authorization');
 
@@ -12,5 +12,8 @@ router.patch('/owners/approve/:id', verifySuperAdminToken, isSuperAdmin, approve
 
 // Reject a specific owner by ID
 router.patch('/owners/reject/:id', verifySuperAdminToken, isSuperAdmin, rejectOwner);
+
+// Get All Aproved List ANd Rejected List
+router.get('/owners/approved', verifySuperAdminToken, isSuperAdmin, getApprovedOwners);
 
 module.exports = router;
