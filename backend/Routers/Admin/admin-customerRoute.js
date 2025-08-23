b0460624c2ee44e1cf0fc654a27adb6c12d13da2
Rishini Nawodi <123630889+rishininawodi@users.noreply.github.com>
2025-08-23 11:20:23 +0530

    add routes to Inquaries controllers

--- backend/Routers/Admin/admin-customerRoute.js
+++ backend/Routers/Admin/admin-customerRoute.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const { getAllCustomers, countCustomers } = require('../../controllers/Admin/admin-customerController');
+const { getAllCustomers, countCustomers,getAllInquiries,getInquiryById,countInquiries } = require('../../controllers/Admin/admin-customerController');
 const { verifySuperAdminToken } = require('../../middleware/Auth/verifyToken');
 const { isSuperAdmin } = require('../../middleware/Auth/authorization');
 
 
 router.get('/customers', verifySuperAdminToken, isSuperAdmin, getAllCustomers);
 router.get('/customers/count', verifySuperAdminToken, isSuperAdmin, countCustomers);
+// Inquiry routes
+router.get('/customers/inquiries', verifySuperAdminToken, isSuperAdmin, getAllInquiries);
+router.get('/customers/inquiries/count', verifySuperAdminToken, isSuperAdmin, countInquiries);
+router.get('/customers/inquiries/:id', verifySuperAdminToken, isSuperAdmin, getInquiryById);
 
 
 module.exports = router;
